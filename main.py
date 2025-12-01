b238129bdb08642d603a340f8fe4aa3a6e3afc00
blazesakharov <blaze.sakharov@gmail.com>
2025-12-01 15:33:14 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import asyncio
 import discord
 from discord import app_commands
 from discord.ext import commands
@@ -5,7 +6,7 @@ import json
 import os
 import asyncio
 
-TOKEN = os.getenv("TOKEN")
+TOKEN = "MTQ0NDcyNjQyMDk3MzgxNzkzOQ.Ga5cX3.r-baZh_zQLX7iABCAeO1j_l57ZumHH820-IVrA"
 
 CONFIG_FILE = "config.json"
 POSTS_FILE = "posts_map.json"
@@ -310,3 +311,4 @@ async def buyer_role_create(interaction: discord.Interaction):
 # ================= RUN =================
 
 bot.run(TOKEN)
+
